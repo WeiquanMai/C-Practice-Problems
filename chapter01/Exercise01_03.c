4b5f3cf87dfcf99e9cca83dd065041c03f8c8f9f
WeiquanMai <weiquanmai@gmail.com>
2026-02-05 16:12:35 -0500

    Create Exercise01_03.c

---
+++ chapter01/Exercise01_03.c
@@ -0,0 +1,29 @@
+ /**
+  * @author Weiquan Mai
+  * @date February 5, 2026
+  * Problem 1.3
+  * Modify the temperature conversion program to print a heading above the table.
+  */
+ #include <stdio.h>
+
+ // Print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300;
+ main(){
+    // Variables
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    // Print heading
+    printf("Fahrenheit Celsius\n");
+
+    while(fahr <= upper){
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%10.0f %7.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+ }
