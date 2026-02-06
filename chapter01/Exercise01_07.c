eb2fb7401f5a84e4e3d6b3388ac0ccab67d0d873
WeiquanMai <weiquanmai@gmail.com>
2026-02-06 17:33:44 -0500

    Create Exercise01_07.c

---
+++ chapter01/Exercise01_07.c
@@ -0,0 +1,13 @@
+/**
+ * @author Weiquan Mai
+ * @date February 6, 2026
+ * Problem 1.7
+ * Write a program to print the value of EOF
+ */
+
+ #include <stdio.h>
+
+ int main(){
+    int i = EOF;
+    printf("%d\n", i);
+ }
